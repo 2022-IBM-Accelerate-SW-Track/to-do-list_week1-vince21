42a32724d96e3c2b536539436c66b9d0071e1bfe
vince21 <vincewils11@gmail.com>
2022-06-12 18:22:54 -0700

    finished week 1 lab

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_picture from '../assets/headshot.jpg';
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
-      </div>
+       <div class="split left">
+         <div className="centered">
+           <img
+             className="profile_image"
+             src={profile_picture}
+             alt="Profile Pic"
+             ></img>
+         </div>
+       </div>
+       <div className="split right">
+         <div className="centered">
+           <div className="name_title">Vincent Wilson</div>
+           <div className="brief_description">
+             Rising Junior studying Computer Science at the University
+             of Washington
+           </div>
+         </div>
+       </div>
+     </div>
     )
   }
 }
